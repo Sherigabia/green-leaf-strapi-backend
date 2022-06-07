8338174fbed72d5763cd4736c163cf708a42a3fe
Sherigabia <orionsavvy@gmail.com>
2022-06-07 23:24:57 +0000

    updated yarn lock

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['DvD1GKKM4lZZ8rt9n1uxXw==','g5t5hC4PeauzmIyhcDaTnw==', 'kXcaDfWqjqrqscxYp7omSA==', 'dnWSdNFotF1d7gth0CRQdQ==']),
   },
 });
